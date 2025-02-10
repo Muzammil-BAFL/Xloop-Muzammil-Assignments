b6975f7b8c600ae18543c8fb1d92e7709c35ee2e
Muzammil-BAFL <roshan.muzammil@gmail.com>
2025-02-10 13:43:58 +0500

    Assignment 4.2 uploaded

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+
+import UserProfile from './screens/UserProfile';
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+          
+        <Stack.Screen name="UserProfile" component={UserProfile} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
